018d58d6f6eda7b93cb7f4804dcfb2fbf9f6a701
csant26 <shishantstha@gmail.com>
2024-10-24 17:11:59 +0545

    commit

---
+++ backend/Data/CampusBridgeDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class CampusBridgeDbContext : DbContext
+    {
+        public CampusBridgeDbContext(DbContextOptions<CampusBridgeDbContext> options):base(options) { }
+    }
+}
